dd5a4bf35ba0edb7cb9aa83fd922d76f34f307bc
Warship55 <lungudumitru55@gmail.com>
2024-03-13 13:28:00 +0200

    add lab3

---
+++ Lab_3/MRSUTWeb-Business_Logic/FirstProj/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstProj.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Deposits()
+        {
+            return View();
+        }
+        public ActionResult DepositMoneyPlus()
+        {
+            return View();
+        }
+        public ActionResult DepositMoneyUp()
+        {
+            return View();
+        }
+        public ActionResult DepositMoneyMegaGain()
+        {
+            return View();
+        }
+        public ActionResult DepositRiseBusiness()
+        {
+            return View();
+        }
+        public ActionResult TermService()
+        {
+            return View();
+        }
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }       
+    }
+}
